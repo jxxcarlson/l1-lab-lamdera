889cc29f188110e0815cb6f337eea717fefc66d5
James Carlson <jxxcarlson@gmail.com>
2021-07-26 16:33:26 -0400

    Preparing for v1

---
+++ src/Evergreen/V1/Document.elm
@@ -0,0 +1,29 @@
+module Evergreen.V1.Document exposing (..)
+
+import Time
+
+
+type alias Username =
+    String
+
+
+type Access
+    = Public
+    | Private
+    | Shared
+        { canRead : List Username
+        , canWrite : List Username
+        }
+
+
+type alias Document =
+    { title : String
+    , author : String
+    , username : String
+    , id : String
+    , created : Time.Posix
+    , modified : Time.Posix
+    , tags : List String
+    , content : String
+    , access : Access
+    }
