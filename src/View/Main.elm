8dc4d1e7dac0c6f0c75ecebf0acb9e72048dcf5a
James Carlson <jxxcarlson@gmail.com>
2021-08-02 14:27:58 -0400

    Put full URL of current doecuemnt in footer

--- src/View/Main.elm
+++ src/View/Main.elm
@@ -64,7 +64,7 @@ footer model =
 
         --, Button.printToPDF model
         , E.el [ Font.size 12, Font.color Color.white, E.paddingXY 10 0 ]
-            (E.text (model.currentDocument |> .slug |> Maybe.withDefault "SLUG" |> (\x -> "/g/" ++ x)))
+            (E.text (model.currentDocument |> .slug |> Maybe.withDefault "SLUG" |> (\x -> "https://l1-lab.lamdera.app/g/" ++ x)))
         , messageRow model
         ]
 
